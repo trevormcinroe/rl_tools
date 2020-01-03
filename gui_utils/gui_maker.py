5b9dc2bbc2579ccecce56c1202ab3735cdfb3843
trevormcinroe <ThomasBayes69!>
2020-01-02 21:32:39 -0600

    adding in robotics utils

--- gui_utils/gui_maker.py
+++ gui_utils/gui_maker.py
@@ -1,89 +1,22 @@
 """
 
 """
-# import tkinter
-#
-#
-# class Window(tkinter.Frame):
-#     def __init__(self, master=None):
-#         tkinter.Frame.__init__(self, master)
-#         self.master = master
-#
-#         menu = tkinter.Menu(self.master)
-#         self.master.config(menu=menu)
-#
-#         file_menu = tkinter.Menu(menu)
-#         file_menu.add_command(label="New Run")
-#         file_menu.add_command(label="Exit", command=self._exit_program)
-#         menu.add_cascade(label="File", menu=file_menu)
-#
-#
-#
-#     def _exit_program(self):
-#         exit()
-#
-#
-# root = tkinter.Tk()
-# app = Window(root)
-# root.wm_title("Tkinter window")
-# root.mainloop()
-
 
 import tkinter as tk
 import numpy as np
 
-class Window():
-
-    def __init__(self, master=None):
-        # tk.Frame.__init__(self, master)
-        self.canvas = None
-
-    def place_robot(self, root):
-        """"""
-        # Our grid is 10x10
-        start = np.random.choice([x for x in range(100)])
-
-        robot = tk.Text(root, height=10, width=10)
-        robot.pack()
-        robot.insert(tk.END, 'R')
-
-    def create_grid(self, event=None):
-        """"""
-        w = self.canvas.winfo_width()  # Get current width of canvas
-        h = self.canvas.winfo_height()  # Get current height of canvas
-        self.canvas.delete('grid_line')  # Will only remove the grid_line
-
-        # Creates all vertical lines at intevals of 100
-        for i in range(0, w, 100):
-            self.canvas.create_line([(i, 0), (i, h)], tag='grid_line')
-
-        # Creates all horizontal lines at intevals of 100
-        for i in range(0, h, 100):
-            self.canvas.create_line([(0, i), (w, i)], tag='grid_line')
-
-    def run(self):
-        root = tk.Tk()
-
-        self.canvas = tk.Canvas(root, height=1000, width=1000, bg='white')
-
-        self.canvas.pack(fill=tk.BOTH, expand=True)
-
-        self.canvas.bind('<Configure>', self.create_grid)
-
-        self.place_robot(root=root)
-
-        root.mainloop()
-
-# a = Window()
-# a.run()
 
 class ArrayGui:
 
-    def __init__(self, height, width):
+    def __init__(self, height, width, num_obstacles):
         self.height = height
         self.width = width
+        self.num_obstacles = num_obstacles
         self.grid = None
         self.robot_position = None
+        self.obstacle_positions = None
+        self.goal_position = None
+        self.robot_orientation = None
 
     def _convert(self):
         """"""
@@ -105,6 +38,7 @@ class ArrayGui:
         robot_position.append(np.random.choice(self.height))
         self.robot_position = robot_position
         self.grid[robot_position[0], robot_position[1]] = 'R'
+        self.robot_orientation = 'north'
 
     def place_obstacles(self, num_obstacles):
         """"""
@@ -124,17 +58,234 @@ class ArrayGui:
         for ob in obstacles:
             self.grid[ob[0], ob[1]] = 'O'
 
-    def read_lidar(self):
+        self.obstacle_positions = obstacles
+
+    def place_goal(self):
         """"""
-        pass
+        invalid = True
+
+        while invalid:
+            holder = []
+            holder.append(np.random.choice(self.width))
+            holder.append(np.random.choice(self.height))
+
+            if not holder == self.robot_position:
+                if len([x for x in self.obstacle_positions if x == holder]) == 0:
+                    invalid = False
+
+        self.grid[holder[0], holder[1]] = 'G'
+        self.goal_position = holder
+
+    def random_lidar(self, dist_category):
+        """
+        Spaces away
+        1: [10, 25]
+        2: [26, 60]
+        3: [61, 130]
+        4: [131, 400]
+
+        Args:
+            dist_category:
+
+        Returns:
+
+        """
+
+        if dist_category == 1:
+            return np.random.randint(10, 25)
+        if dist_category == 2:
+            return np.random.randint(26, 60)
+        if dist_category == 3:
+            return np.random.randint(61, 130)
+        if dist_category == 4:
+            return np.random.randint(131, 400)
+
+    def lidar_left(self):
+        """"""
+
+        spots_looked = 1
+        obstacle_found = False
+
+        while not obstacle_found:
+            if spots_looked == 4:
+                # If we are here in the logic flow, we have looked four spots to the left and found nothing
+                # Therefore our sensor should max out!
+                left_sensor_data = 400
+                obstacle_found = True
+
+            if self.robot_position[1] - spots_looked < 0:
+                # If we are here in the logic flow, we have hit a wall
+                left_sensor_data = self.random_lidar(dist_category=spots_looked)
+                obstacle_found = True
+
+            # If we are here in the logic flow, we have neither reached the max number of spots scanned
+            # NOR have we hit a wall
+            spot_looking = [self.robot_position[0], self.robot_position[1] - spots_looked]
+
+            if spot_looking in self.obstacle_positions:
+                # If here, then there is an obstacle in our current scanned spot!
+                left_sensor_data = self.random_lidar(dist_category=spots_looked)
+                obstacle_found = True
+
+            # Incrementing as none of the above have returned anything
+            spots_looked += 1
+
+        return left_sensor_data
+
+    def lidar_right(self):
+        """"""
+
+        spots_looked = 1
+        obstacle_found = False
+
+        while not obstacle_found:
+            if spots_looked == 4:
+                # If we are here in the logic flow, we have looked four spots to the right and found nothing
+                # Therefore our sensor should max out!
+                right_sensor_data = 400
+                obstacle_found = True
+
+            if self.robot_position[1] + spots_looked >= self.width:
+                # If we are here in the logic flow, we have hit a wall
+                right_sensor_data = self.random_lidar(dist_category=spots_looked)
+                obstacle_found = True
+
+            # If we are here in the logic flow, we have neither reached the max number of spots scanned
+            # NOR have we hit a wall
+            spot_looking = [self.robot_position[0], self.robot_position[1] + spots_looked]
+
+            if spot_looking in self.obstacle_positions:
+                # If here, then there is an obstacle in our current scanned spot!
+                right_sensor_data = self.random_lidar(dist_category=spots_looked)
+                obstacle_found = True
+
+            # Incrementing as none of the above have returned anything
+            spots_looked += 1
+
+        return right_sensor_data
+
+    def lidar_up(self):
+        """"""
+
+        spots_looked = 1
+        obstacle_found = False
+
+        while not obstacle_found:
+            if spots_looked ==4 :
+                # If we are here in the logic flow, we have looked four spots to the right and found nothing
+                # Therefore our sensor should max out!
+                up_sensor_data = 400
+                obstacle_found = True
+
+            if self.robot_position[0] - spots_looked < 0:
+                # If we are here in the logic flow, we have hit a wall
+                up_sensor_data = self.random_lidar(dist_category=spots_looked)
+                obstacle_found = True
+
+            # If we are here in the logic flow, we have neither reached the max number of spots scanned
+            # NOR have we hit a wall
+            spot_looking = [self.robot_position[0] - spots_looked, self.robot_position[1]]
+
+            if spot_looking in self.obstacle_positions:
+                # If here, then there is an obstacle in our current scanned spot!
+                up_sensor_data = self.random_lidar(dist_category=spots_looked)
+                obstacle_found = True
+
+            # Incrementing as none of the above have returned anything
+            spots_looked += 1
+
+        return up_sensor_data
+
+    def lidar_down(self):
+        """"""
+
+        spots_looked = 1
+        obstacle_found = False
+
+        while not obstacle_found:
+            if spots_looked == 4:
+                # If we are here in the logic flow, we have looked four spots to the right and found nothing
+                # Therefore our sensor should max out!
+                down_sensor_data = 400
+                obstacle_found = True
+
+            if self.robot_position[0] + spots_looked >= self.height:
+                # If we are here in the logic flow, we have hit a wall
+                down_sensor_data = self.random_lidar(dist_category=spots_looked)
+                obstacle_found = True
+
+            # If we are here in the logic flow, we have neither reached the max number of spots scanned
+            # NOR have we hit a wall
+            spot_looking = [self.robot_position[0] + spots_looked, self.robot_position[1]]
+
+            if spot_looking in self.obstacle_positions:
+                # If here, then there is an obstacle in our current scanned spot!
+                down_sensor_data = self.random_lidar(dist_category=spots_looked)
+                obstacle_found = True
+
+            # Incrementing as none of the above have returned anything
+            spots_looked += 1
+
+        return down_sensor_data
+
+
+    def read_lidar(self):
+        """
+        The scanning should work by looking, at most 4 places in each direction until either an obstacle or
+        wall is hit!
+
+        Left/Right deal with column changes
+        Up/Down deal with row changes
+
+        The purpose of this method is to handle routing to the proper lidar reading methods based on the robot's
+        current orientation
+
+
+
+        Returns:
+
+        """
+        data = {
+            'agent_gps': self.robot_position,
+            'goal_gps': self.goal_position,
+            'left_lidar': None,
+            'right_lidat': None,
+            'up_lidar': None,
+            'down_lidar': None
+        }
+
+        if self.robot_orientation == 'north':
+            data['left_lidar'] = self.lidar_left()
+            data['right_lidat'] = self.lidar_right()
+            data['up_lidar'] = self.lidar_up()
+            data['down_lidar'] = self.lidar_down()
+
+        if self.robot_orientation == 'south':
+            pass
+
+        if self.robot_orientation == 'east':
+            pass
+
+        if self.robot_orientation == 'west'
 
     def go(self):
         self._convert()
         self.place_robot()
-        self.place_obstacles(num_obstacles=10)
+        self.place_obstacles(num_obstacles=self.num_obstacles)
+        self.place_goal()
         print(self.grid)
 
 
 
-a = ArrayGui(height=15, width=15)
+a = ArrayGui(height=15, width=15, num_obstacles=40)
 a.go()
+
+print(a.lidar_left())
+print(a.lidar_right())
+print(a.lidar_up())
+print(a.lidar_down())
+
+# 1: [10, 25]
+# # 2: [26, 60]
+# # 3: [61, 130]
+# # 4: [131, 400]
